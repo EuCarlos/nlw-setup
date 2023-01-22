465266cceec4d60808609b9d1e0f4e19a70c0633
Carlos Alves <carlosalves369m@hotmail.com>
2023-01-22 10:24:32 -0300

    :heavy_plus_sign: configure cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
 import Fastify from "fastify";
 import { PrismaClient } from '@prisma/client';
+import cors from '@fastify/cors'
 
 const app =  Fastify();
 const prisma = new PrismaClient();
+
+// const allowedHosts= { origin: ['http://localhost:3000'] }
+app.register(cors); // (cors, allowedHosts)
 
 app.get('/', async () => {
     const habits = await prisma.habit.findMany();
