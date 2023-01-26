386785c96afc56e48e5a39516bb5f737039c1bb5
Carlos Alves <carlosalves369m@hotmail.com>
2023-01-26 10:37:02 -0300

    :dizzy: add transitions and focus

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -25,7 +25,7 @@ export function HabitDay ({ defaultCompleted = 0, amount = 0, date }: IHabit) {
     return (
         <Popover.Root>
             <Popover.Trigger 
-                className={clsx("w-10 h-10 border-2 rounded-lg", {
+                className={clsx("w-10 h-10 border-2 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-background", {
                     "bg-zinc-900 border-zinc-800": completedPercentage === 0,
                     "bg-violet-900 border-violet-500": completedPercentage > 0 && completedPercentage < 20,
                     "bg-violet-800 border-violet-500": completedPercentage >= 20 && completedPercentage < 40,
